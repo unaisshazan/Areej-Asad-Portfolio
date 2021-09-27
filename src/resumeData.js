bf83b7760ca9ed8302e57445f686d4fe874a383e
areejasad98 <54935960+areejasad98@users.noreply.github.com>
2021-09-27 23:35:41 +0500

    Areej

---
+++ src/resumeData.js
@@ -0,0 +1,239 @@
+let resumeData = {
+    "imagebaseurl":"https://i.ibb.co/N2bcx9m/Areej-Photo.jpg",
+    "name": "Areej Asad",
+    "role": "Frontend Developer and Electronic Engineer",
+    "linkedinId":" areejasad16@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/areej-asad-b31324169/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/areejasad98",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/AreejAsad5",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a graduate Electronic Engineeer from NED University of Engineering & Technology. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Pakistan",
+    "website":"https://areejasad.com",
+    "education":[
+      {
+        "UniversityName":"NED University Of Engineering & Technology",
+        "specialization":"Graduate Electronic Engineer",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Cordoba School",
+        "specialization":"A'levels",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"AES School",
+        "specialization":"O'levels",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":""
+      }
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Laar Science Festival, Thatta",
+        "specialization":"Volunteer",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2018",
+        "Achievements":"Worked as a volunteer in kids science exhibition"
+      },
+      {
+        "CompanyName":"SENTEC, NEDUET, Karachi",
+        "specialization":"Robotics Member",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked as an active member of Team Robotics in SENTEC, The Society For Promotion of Science, Engineering & Technology and conducted various robotic competitions."
+      },
+      {
+        "CompanyName":"Instrumentation Center, NEDUET, Karachi",
+        "specialization":"Internee",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked as an internee, developed many projects"
+      },
+      {
+        "CompanyName":"Neurocomputation (NCAI) Lab, NEDUET, Karachi",
+        "specialization":"Internee",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":"Planned and created STEM education course for schools. Along with that, also conducted various workshops including at NED as well as Beaconhouse School."
+      },
+      {
+        "CompanyName":"Munphurid, Karachi",
+        "specialization":"Instructor",
+        "MonthOfLeaving":"Apr",
+        "YearOfLeaving":"2020",
+        "Achievements":"As an instructor for Android Development For Kids"
+      },
+      {
+        "CompanyName":"U-TECH Digital Education, Karachi",
+        "specialization":"Co-owner & Instructor",
+        "MonthOfLeaving":"Apr",
+        "YearOfLeaving":"2020",
+        "Achievements":"Launched my own startup with two of my colleagues and managed to win SHELL Tameer Business Pitch in May, 2021."
+      },
+      {
+        "CompanyName":"NAVTCC, NEDUET, Karachi",
+        "specialization":"Instructor",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2021",
+        "Achievements":"Worked as instructor in Prime Minister's Kamyab Jawan Program"
+      },
+      {
+        "CompanyName":"U-CADEMY, Karachi",
+        "specialization":"CEO",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2021",
+        "Achievements":"Launched another startup."
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"CMS"
+      },
+      {
+        "skillname":"PCB Designing"
+      },
+      {
+        "skillname":"C Language"
+      },
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"Python Programming"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Robotic Arm",
+        "description":"Arduino Based Robotic Arm was developed using servos as joints for arm.",
+        "imgurl":"images/portfolio/robotic-arm.jpeg"
+      },
+      {
+        "name":"Color Sorter",
+        "description":"An Arduino Based Color Sorter was made and displayed in exhibition. The Color Sorter, detected different colors & sorted those colored marbels into th",
+        "imgurl":"images/portfolio/Color-sorter.jpeg"
+      },
+      {
+        "name":"Raspberry Pi Minibot & Home Automation",
+        "description":"The Raspberry Pi Minibot is chatbot as well as home automation device that delivers output on the basis of voice command.It can be used for security purposes as it detects a persons movement using motion sensor and alerts by the help of buzzer if there is any intruder in the house.",  
+        "imgurl":"images/portfolio/raspi.jpeg"
+      },
+      {
+        "name":"Line Following Robot",
+        "description":"LFR is the electronic robot thats follows white path on a black surface and stops when it detects obstacle on its path",
+        "imgurl":"images/portfolio/lfr.jpg"
+      },
+      {
+        "name":"RC Car with Bluetooth",
+        "description":"RC Car is Remote Control Car that is made using Arduino and Bluetooth Module. An Android App was developed that controlled the car.",  
+        "imgurl":"images/portfolio/RC Car.jpg"
+      },
+      {
+        "name":"AIML Chatbot",
+        "description":"The aiml is a fast technique chat bot made using python programing .It used to generate automatic responses.",  
+        "imgurl":"images/portfolio/chatbot-3.png"
+      },
+      {
+        "name":"Hand written Digit Recognition",
+        "description":"The Machine Learning based project that detects hand written digits and converts it into digital written text.",  
+        "imgurl":"images/portfolio/hwi.png"
+      },
+      {
+        "name":"Image to text",
+        "description":"The image to text project basically a Machine Learning project where text will be extracted from images.",  
+        "imgurl":"images/portfolio/image to text.jpg"
+      },
+      {
+        "name":"Persistence of Vision Display",
+        "description":"POV Display is a kind of optical illusion using which we can dsiplay any text or image.",  
+        "imgurl":"images/portfolio/pov.jfif"
+      },
+      {
+        "name":"Reverse Engineering of Mehhran Fan PCB Circuit",
+        "description":"A complete schematic was developed of fan circuit on Multisim by Reverse Engineering method.",  
+        "imgurl":"images/portfolio/re.jpg"
+      },
+      {
+        "name":"DC motor speed control using temperature sensor",
+        "description":"A circuit was developed that controlled the spped of a DC motor with respect to the temperature.",  
+        "imgurl":"images/portfolio/DC motor.jpeg"
+      },
+      {
+        "name":"Audio Amplifer with noise reduction using MATLAB",
+        "description":"A circuit was developed that controlled the spped of a DC motor with respect to the temperature.",  
+        "imgurl":"images/portfolio/matlab.png"
+      },
+      {
+        "name":"Digital Clock on FPGA",
+        "description":"A digital clock was implemented using FPGA and 7-Segment display.",  
+        "imgurl":"images/portfolio/digital clock.jpg"
+      },
+      {
+        "name":"Sentiment Analysis Feedback System & Attendance System",
+        "description":"A Jetson Nano based device that detects face and sentiment of a person through facial expressions and generated feedback. It also works as Smart Attendance System.",  
+        "imgurl":"images/portfolio/SA.jpg"
+      },
+      {
+        "name":"Evrimo Website",
+        "description":"Developed Evrimo Website using Elementor Pro tools",  
+        "imgurl":"images/portfolio/evrimo.jpeg"
+      },
+      {
+        "name":"JavaScript Project - To do list",
+        "description":"A to do list website was deveoped using JavaScript",  
+        "imgurl":"images/portfolio/to do list.jpeg"
+      },
+      {
+        "name":"JavaScript Project - Caculator",
+        "description":"A calculator website was deveoped using JavaScript",  
+        "imgurl":"images/portfolio/calculator.jpeg"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
